4a902ca4d1f09634c1caac7e2bbc11f8499a8c6c
deveyNull <m171458@usna.edu>
2017-04-05 08:25:32 -0500

    aggregator add, some other stuff.

--- aggregator.py
+++ aggregator.py
@@ -1,4 +1,5 @@
 import os
+from subprocess import Popen, PIPE, STDOUT
 import argparse
 import operator
 import time
@@ -9,7 +10,8 @@ import pandas as pd
 
 parser = argparse.ArgumentParser(description="provide the parameters to make the file run")
 parser.add_argument("-r", "--read", help='Filename of the dir to read', required=True)
-parser.add_argument('-w', '--write', help='Filename of the csv to write', required=True)
+parser.add_argument('-wt', '--writeTcp', help='Filename of the csv to write', required=True)
+parser.add_argument('-wd', '--writeDns', help='Filename of the csv to write', required=True)
 parser.add_argument('-t', '--time', help='Time length to split by if desired', required=False, default='std')
 parser.add_argument('-l', '--label', help='Label for dataset', required=True)
 parser.add_argument('-d', '--dataset', help='Dataset name', required=True)
@@ -28,7 +30,9 @@ dataset = args['dataset']
 owner = args['owner']
 target = args['target']
 
-outputCsv = args['write']
+outputCsvTcp = args['writeTcp']
+outputCsvDns = args['writeDns']
+
 
 timer = args['time']
 string = ""
@@ -41,18 +45,47 @@ if args['target']:
 
 count = 1
 
-allFiles = glob.glob(dirName+ "/*.pcap*")
+allFiles = glob.glob(dirName+ "/*/*.bz2")
 
 for i in allFiles:
-    print('unBzip '+i)
+    cmd = 'bzip2 -dk '+i
+    #p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    print("****" + cmd)
+    #print(p).stdout.readlines()
+    #newT = time.time()
+    #print(newT - start)
     fileName = i.rsplit('.', 1)[:-1][0]
-    print('tshark -r' + fileName + "-R ip.addr==1.1.1.1 or ip.addr==1.1.1.1 or ip.addr==1.1.1.1 or ip.addr==1.1.1.1 -w temporary.pcap")
-
-    print("python pcapToTcpCsv.py -r temporary.pcap -w tempTcp.csv")
-    print("python csvTcpToPandas.py -r tempTcp.csv -w " + outputCsv + " -l "+ label + " -d " + dataset + " -o " + owner + string)
-    print("python pcapToDnsCsv.py -r -r temporary.pcap -w tempDns.csv")
-    print("python csvDnsToPandas.py -r tempDns.csv -w " + outputCsv + " -l "+ label + " -d " + dataset + " -o " + owner + string)
-    print("python remove temporary.pcap")
+    #dirPath = i.rsplit('/', 1)[:-1][0]
+    #print(dirPath)
+    cmd = 'tshark -nr '+ fileName + ' -N -Y "(ip.addr== 10.1.70.20) and  (ip.addr!= 10.1.70.40 or ip.addr!= 10.1.70.41 or ip.addr!= 10.1.70.42 or ip.addr!= 10.1.70.43) and (! ldap or ! ftp or ! tpkt) and (dns or tcp)" -w temporary.pcap'
+    print("****" + cmd)
+    #p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    #print(p).stdout.readlines()
+    #newT = time.time()
+    #print(newT - start)
+    cmd = "python pcapToTcpCsv.py -r temporary.pcap -w tempTcp.csv"
+    #p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    print("****" + cmd)
+    #print(p).stdout.readlines()
+    cmd = "python csvTcpToPandas.py -r tempTcp.csv -w "+ outputCsvTcp + " -l "+ label + " -d " + dataset + " -o " + owner + string
+    #p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    print("****" + cmd)
+    #print(p).stdout.readlines()
+    #newT = time.time()
+    #print(newT - start)
+    cmd = "python pcapToDnsCsv.py -r temporary.pcap -w tempDns.csv"
+    #p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    print("****" + cmd)
+    #print(p).stdout.readlines()
+    #newT = time.time()
+    #print(newT - start)
+    cmd = "python csvDnsToPandas.py -r tempDns.csv -w " + outputCsvDns + " -l "+ label + " -d " + dataset + " -o " + owner + string
+    #p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    print("****" + cmd)
+    #print(p).stdout.readlines()
+    #newT = time.time()
+    #print(newT - start)
+   # print("python remove temporary.pcap")
     print("python remove tempTcp.csv")
     print("python remove tempDns.csv")
 
